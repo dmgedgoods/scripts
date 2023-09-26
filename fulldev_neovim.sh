6eabaa3b4c074ea6b2b184421c1cb541281e3b1c
dmgedgoods <119913388+dmgedgoods@users.noreply.github.com>
2023-09-26 15:58:46 -0700

    Update fulldev_neovim.sh

--- fulldev_neovim.sh
+++ fulldev_neovim.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# Basic Neovim dev setup. Debian raw install. Edit to your distro.
+
 sudo apt update && sudo apt upgrade -y
 
-sudo apt-get install -y curl tree zsh git tldr fzf unzip gh gcc unzip python3.10-venv pip openjdk-18-jre openjdk-18-jdk
+sudo apt install -y wget curl tmux tree zsh git tldr fzf unzip gh gcc unzip python3-venv python3-pip default-jdk default-jre 
 
 sudo wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 sudo tar -zxvf nvim-linux64.tar.gz -C /opt/
@@ -15,16 +17,16 @@ sudo ln -s /opt/node-v18.17.1-linux-x64/bin/node /usr/bin/node
 sudo ln -s /opt/node-v18.17.1-linux-x64/bin/npm /usr/bin/npm
 sudo ln -s /opt/node-v18.17.1-linux-x64/bin/npx /usr/bin/npx
 sudo rm node-v18.17.1-linux-x64.tar.xz
-rm -rf ~/.config/nvim && rm -rf ~/.local/share/nvim
+
+rm -rf ~/.config/nvim && rm -rf ~/.local/share/nvim # Don't nuke the whole config folder
+mkdir -p ~/.config # Just in case
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 git clone https://github.com/dmgedgoods/dotfiles.git
-mkdir -p ~/.config
-ln -s ~/dotfiles/.config/nvim ~/.config
+ln -s ~/dotfiles/.config/nvim/ ~/.config
 ln -s ~/dotfiles/.config/tmux ~/.config
-ln -s ~/dotfiles/.local/bin ~/.local/
-nvim  #fix for :so and :PackerSync after initial install
+nvim --headless -u NONE ~/.config/nvim/lua/dmgedgoods/packer.lua "+so" "+PackerSync" "+sleep 10" "+qa"
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
