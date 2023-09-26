5f076a6f03fe84e4d4cb5323afadd95dcfbe7399
dmgedgoods <119913388+dmgedgoods@users.noreply.github.com>
2023-09-26 09:42:51 -0700

    Update tmux_dev.sh

--- tmux_dev.sh
+++ tmux_dev.sh
@@ -5,13 +5,13 @@ tmux has-session -t dev 2>/dev/null
 
 # $? is a special variable that holds the exit status of the last command executed
 if [ $? != 0 ]; then
-  # Create a new session with the first window named 'nvim'
-  tmux new-session -s dev:1 -n nvim -d
+  # Create a new session with the first
+  tmux new-session -s dev:1 -d
 
   # Create the other windows
-  tmux new-window -t dev:2 -n term
-  tmux new-window -t dev:3 -n file
-  tmux new-window -t dev:4 -n misc
+  tmux new-window -t dev:2 
+  tmux new-window -t dev:3 
+  tmux new-window -t dev:4 
   
   # Attach to the session
   tmux attach -t dev
